2c25cda228415794209e7a21a8ea056f0c9692a6
carlos <cgonzalez.mx@outlook.com>
2026-05-05 09:03:47 -0600

    fix: change /home to just / in routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ChecklistController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function() {
-    Route::get('/home', [ChecklistController::class, 'index']);
+    Route::get('/', [ChecklistController::class, 'index']);
     Route::post('/checklists', [ChecklistController::class, 'store'])->name('checklists.post');
     Route::get('/checklists/edit/{checklist}', [ChecklistController::class, 'edit'])->name('checklists.edit');
 });
