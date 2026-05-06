6c46274de34ccd14b220064704667ed86f1b80b8
carlos <cgonzalez.mx@outlook.com>
2026-05-06 11:43:21 -0600

    refactor: add more precise tiping to event

--- resources/js/components/InputPassword.svelte
+++ resources/js/components/InputPassword.svelte
@@ -6,7 +6,7 @@ let { value = $bindable(''), label } = $props();
 let is_secret = $state(true);
 const type = $derived(is_secret ? 'password' : 'text')
 const id = nanoid();
-const toggleSecret = (e: Event) => {
+const toggleSecret = (e: MouseEvent) => {
     e.preventDefault();
     is_secret = !is_secret;
 };
