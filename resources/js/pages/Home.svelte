47d39738c689c0d8d82365fdbe9c99b82a19d520
carlos <cgonzalez.mx@outlook.com>
2026-05-07 11:02:41 -0600

    style: improve ui

--- resources/js/pages/Home.svelte
+++ resources/js/pages/Home.svelte
@@ -14,7 +14,7 @@ let { checklists }: Props = $props();
 
 <AppLayout>
     <div class="w-full overflow-auto max-h-full">
-        <div class="flex justify-between py-4 w-[95vw] xl:w-2/3 sticky top-0 bg-slate-100/30 backdrop-blur-sm">
+        <div class="flex justify-between pt-4 pb-10 w-[95vw] xl:w-2/3 sticky top-0 bg-linear-to-t from-transparent from-10% to-slate-100 to-30%">
             <div>
                 <Link href="checklists" as="button" method="post" class="p-2 rounded bg-indigo-500 text-slate-50 font-medium ">
                     Crear lista
@@ -23,8 +23,8 @@ let { checklists }: Props = $props();
             <div></div>
             <div></div>
         </div>
-        <div class="w-[95vw] xl:w-2/3">
-            <ul>
+        <div class="w-[95vw] xl:w-2/3 flex justify-center">
+            <ul class="w-[90%]">
                 {#each checklists as checklist, i (checklist.id)}
                     <li class="mb-3">
                         <div class="flex rounded shadow bg-white overflow-hidden">
