670871aaca757b96bcc5275cf613b75a581727ca
carlos <cgonzalez.mx@outlook.com>
2026-05-06 18:31:37 -0600

    feat: implement debounce action

--- resources/js/pages/checklists/EditChecklist.svelte
+++ resources/js/pages/checklists/EditChecklist.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 import type { Checklist } from "@/types/checklist";
-import { router } from "@inertiajs/svelte";
-import { debounce } from "ts-debounce";
+import { router, Link } from "@inertiajs/svelte";
 import { ulid } from "ulid";
 import { formatDistanceToNow } from "date-fns";
 import { es } from 'date-fns/locale'
+import { debounce } from "ts-debounce";
+import { debounceInput } from "@/lib/actions/debounce.svelte";
 
 interface Props {
     checklist: Checklist;
@@ -13,20 +14,20 @@ interface Props {
 let { checklist }: Props = $props();
 const timePassed = () => formatDistanceToNow(checklist.created_at, { locale: es, addSuffix: true});
 
-const updateChecklistTitle = debounce((event: Event) => {
-    const value = (event.target as HTMLInputElement).value;
+function updateChecklistTitle(ev: Event) {
+    const value = (ev.target as HTMLInputElement).value;
     router.patch(`/checklists/${checklist.id}`, {title: value});
-}, 700);
+}
 
 const updateItemTitle = debounce((event: Event, itemId: string) => {
     const value = (event.target as HTMLInputElement).value;
     router.patch(`/items/${itemId}`, {title: value});
-}, 700);
+}, 300);
 
 const updateCheckedState = debounce((event: Event, itemId: string) => {
     const checked = (event.target as HTMLInputElement).checked;
     router.patch(`/items/${itemId}`, {is_checked: checked});
-}, 500);
+}, 300);
 
 function addItem() {
     router.post(`/checklists/${checklist.id}/items`, {
@@ -38,7 +39,8 @@ function addItem() {
 </script>
 
 <div class="flex flex-col w-[95vw] xl:w-2/3 bg-slate-50 rounded shadow mx-auto p-6">
-    <input type="text" value={checklist.title} oninput={updateChecklistTitle} placeholder="Título" class="text-4xl mb-6">
+    <Link href="/">Regresar</Link>
+    <input type="text" value={checklist.title} use:debounceInput={{ callback: updateChecklistTitle }} placeholder="Título" class="text-4xl mb-6">
     <div class="text-slate-500 mb-3">
         {timePassed()}
     </div>
